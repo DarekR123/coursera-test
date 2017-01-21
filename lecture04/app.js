89488258433986578328f5458e484dc6b16f65e0
DarekR <darekr@some.com>
2017-01-21 15:43:50 +0100

    assigment to module 1

--- lecture04/app.js
+++ lecture04/app.js
@@ -6,9 +6,9 @@ var x = "hello";
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function($scope) {
-$scope.name = "DArek";
+$scope.name1 = "Darek";
 $scope.sayHello = function() {
-  return "Hello Coursera!";
+  return "Hello Coursera";
 }
 
 });
---
+++ zadanie/app.js
@@ -0,0 +1,35 @@
+(function () {
+  'use strict';
+
+var x = "hello";
+
+angular.module('myFirstApp', [])
+
+.controller('MyFirstController', function($scope) {
+$scope.food = "";
+$scope.result_txt = "--->";
+$scope.what_food = function() {
+  return "What do you eat for lunch? (enter separately with koma ',' sing)";
+}
+
+$scope.ile = function(){
+  var re = /\s*,\s*/;
+  var nameList=$scope.food.split(re);
+  var counter = 0
+  angular.forEach(nameList, function(value) {
+     if (value) counter++;
+   });
+  console.log(counter);
+  $scope.result(counter);
+};
+
+$scope.result = function(il){
+  if (il >= 4) $scope.result_txt = "Too much!";
+  if (il < 4) $scope.result_txt = "Enjoy!";
+  if (il == 0) $scope.result_txt = "Please enter data first";
+
+};
+
+});
+
+})();
